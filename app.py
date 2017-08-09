dfc2a5a16ca6f40ebbd196fc45ca1610a8ff94ce
Nathan <nathan@Nathans-MacBook-Pro.local>
2017-08-09 00:08:18 -0400

    Fixed for heroku

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,send_from_directory
-
+import os
 
 app = Flask(__name__)
 
@@ -10,3 +10,8 @@ def send_js(path):
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
